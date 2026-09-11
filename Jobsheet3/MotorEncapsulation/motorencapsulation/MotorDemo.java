a22e2c30eac347d034a879de5003afc6769fec52
FarhanL4h <muhanmenmedan06@gmail.com>
2026-09-11 09:52:00 +0700

    Feat: Jobsheet 3 percobaan 3.2

--- Jobsheet3/MotorEncapsulation/motorencapsulation/MotorDemo.java
+++ Jobsheet3/MotorEncapsulation/motorencapsulation/MotorDemo.java
@@ -3,13 +3,20 @@ package motorencapsulation;
 public class MotorDemo {
 
     public static void main(String[] args) {
-
         Motor motor = new Motor();
+        motor.printStatus();
+        motor.tambahKecepatan();
 
+        motor.nyalakanMesin();
         motor.printStatus();
 
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+        motor.printStatus();
+        
+        motor.tambahKecepatan();
+        motor.printStatus();
 
+        motor.matikanMesin();
         motor.printStatus();
     }
 }
