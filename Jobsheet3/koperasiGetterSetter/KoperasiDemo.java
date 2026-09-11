6ddceec5d4b65e9f27d2dae76c810d924de7e3d4
FarhanL4h <muhanmenmedan06@gmail.com>
2026-09-11 10:41:03 +0700

    Feat: Jobsheet 3 Percobaan 3.5

--- Jobsheet3/koperasiGetterSetter/KoperasiDemo.java
+++ Jobsheet3/koperasiGetterSetter/KoperasiDemo.java
@@ -2,14 +2,16 @@ package Jobsheet3.koperasiGetterSetter;
 
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
-        anggota1.setNama("Farhan Tes");
+        Anggota anggota1 = new Anggota("Farhan", "Suhat");
+        // anggota1.setNama("Farhan Tes");
+        System.out.println("Simpanan" + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+        
         anggota1.setAlamat("Suhat");
         anggota1.setor(100000);
-        System.out.println("Simpan" +anggota1.getNama()+ " : Rp " + anggota1.getSimpanan());
-        
+        System.out.println("Simpanan" + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+
         anggota1.pinjam(5000);
 
-        System.out.println("Simpan" +anggota1.getNama()+ " : Rp " + anggota1.getSimpanan());
+        System.out.println("Simpanan" + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
     }
 }
